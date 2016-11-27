0db4f0214fecbb22d7ae719debf7645a27fabd00
sugiyasu kei <igetiwant@gmail.com>
2016-11-27 18:52:28 +0900

    test saveTodo happy pattern

--- typescript/test/saga/main.ts
+++ typescript/test/saga/main.ts
@@ -3,9 +3,14 @@ import kernel from "../../main/di/inversify.config"
 import TYPES from "../../main/di/types";
 import {MainSaga} from "../../main/saga/main";
 import {call, put} from "redux-saga/effects";
+import {HttpClient} from "../../main/http/common";
 
 function getMainSaga():MainSaga {
     return kernel.get<MainSaga>(TYPES.MainSaga)
+}
+
+function getHttpClient():HttpClient {
+    return kernel.get<HttpClient>(TYPES.HttpClient)
 }
 
 const saga:MainSaga = getMainSaga();
@@ -22,6 +27,19 @@ describe("mainSaga", function() {
             const addTodoExecAction:AddTodoExecAction = addTodoExec(nextTodoId, addTodoAction.text)
             const next = iterator.next(nextTodoId);
             expect(next.value).toEqual(put(addTodoExecAction))
-        })
+        });
+    });
+    describe("saveTodo", function(){
+        let iterator:Iterator<any>;
+        const addTodoExecAction:AddTodoExecAction = addTodoExec(1, "test");
+        beforeEach(() => {
+           iterator = saga.saveTodo(addTodoExecAction)
+        });
+        it("call httpClient.post with args and finish", function(){
+            const httpClient = getHttpClient();
+            const callHttpClientPost = iterator.next().value.CALL;
+            expect(callHttpClientPost.fn).toEqual(httpClient.post);
+            expect(iterator.next().done).toBe(true);
+        });
     });
 });
