6ced1d4549ff927f552feed8cd1a54c7d0fbd9c6
sugiyasu kei <igetiwant@gmail.com>
2016-11-27 17:47:38 +0900

    add redux-saga test

--- typescript/test/saga/main.ts
+++ typescript/test/saga/main.ts
@@ -1,8 +1,8 @@
-import {AddTodoAction, addTodo} from "../../main/actions/mainAction";
+import {AddTodoAction, addTodo, AddTodoExecAction, addTodoExec} from "../../main/actions/mainAction";
 import kernel from "../../main/di/inversify.config"
 import TYPES from "../../main/di/types";
 import {MainSaga} from "../../main/saga/main";
-import {call} from "redux-saga/effects";
+import {call, put} from "redux-saga/effects";
 
 function getMainSaga():MainSaga {
     return kernel.get<MainSaga>(TYPES.MainSaga)
@@ -12,10 +12,16 @@ const saga:MainSaga = getMainSaga();
 const addTodoAction:AddTodoAction = addTodo("test");
 
 describe("mainSaga", function() {
-    describe('addTodo', function(){
+    describe("addTodo", function(){
         const iterator = saga.addTodo(addTodoAction);
-        it('call incrementId', function(){
+        it("call incrementId", function(){
             expect(iterator.next().value).toEqual(call(saga.incrementId))
         });
+        it("then, put addTodoExecAction", function () {
+            const nextTodoId = 5;
+            const addTodoExecAction:AddTodoExecAction = addTodoExec(nextTodoId, addTodoAction.text)
+            const next = iterator.next(nextTodoId);
+            expect(next.value).toEqual(put(addTodoExecAction))
+        })
     });
 });
